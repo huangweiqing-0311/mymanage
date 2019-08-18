c0e38bf10a8e0922dc8d6620408c344beac7a4c9
忍者无敌 <weiqing@qq.com>
2019-08-18 20:57:59 +0800

    权限列表的完成

---
+++ my-project/src/views/rights.vue
@@ -0,0 +1,54 @@
+ <template>
+  <div class="my-right">
+    <bread first="权限列表" second="权限列表"></bread>
+
+     <el-table :data="rightData" border style="width: 100%">
+       <el-table-column type="index" label="#"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+      <el-table-column prop="level" label="层级"  width="180"></el-table-column>
+    </el-table>
+  </div>
+</template>
+      
+ <script>
+
+  import {http} from '../api/http.js'
+
+ export default {
+        data() {
+          return {
+              //保存权限列表的数据
+              rightData: {},
+          }
+        },
+
+        created() {
+            http.getRight('list').then(res => {
+                //console.log(res)
+                for(let i = 0; i < res.data.data.length; i++){
+                   //switch这里的要判断全等 ===
+                   //既然要全等, 也要类型一致
+                   switch(res.data.data[i].level){
+                       
+                       case '0':
+                            res.data.data[i].level = '一级'
+                            break
+                       case '1':
+                            res.data.data[i].level = '二级'
+                            break
+                       case '2':
+                             res.data.data[i].level = '三级'
+                             break     
+                    }
+                } 
+                
+                this.rightData = res.data.data
+            })
+        },
+};
+</script>
+ 
+ <style>
+</style>
+ 
---
+++ my-project/src/views/roles.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="my-roles">
+    <break first="权限管理" second="角色列表"></break>
+
+    <!-- 添加栏 -->
+    <el-row>
+      <el-col :span="8">
+        <el-button type="success" @click="addRoles" plain>添加角色</el-button>
+      </el-col>
+    </el-row>
+
+    <!-- 表格 -->
+    <el-table :data="roleForm" :border="true" style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="商品名称">
+              <span>{{ props.row.name }}</span>
+            </el-form-item>
+            <el-form-item label="所属店铺">
+              <span>{{ props.row.shop }}</span>
+            </el-form-item>
+            <el-form-item label="商品 ID">
+              <span>{{ props.row.id }}</span>
+            </el-form-item>
+            <el-form-item label="店铺 ID">
+              <span>{{ props.row.shopId }}</span>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <span>{{ props.row.category }}</span>
+            </el-form-item>
+            <el-form-item label="店铺地址">
+              <span>{{ props.row.address }}</span>
+            </el-form-item>
+            <el-form-item label="商品描述">
+              <span>{{ props.row.desc }}</span>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-table-column>
+      <el-table-column type="index" label="#"></el-table-column>
+      <el-table-column label="角色名称" prop="role_name"></el-table-column>
+      <el-table-column label="角色描述" prop="desc"></el-table-column>
+      <el-table-column label="操作" width="240">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            @click="handleEdit(scope.row)"
+          ></el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            plain
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+          ></el-button>
+          <el-button
+            size="mini"
+            type="warning"
+            plain
+            icon="el-icon-check"
+            @click="handleRole(scope.row)"
+          ></el-button>
+
+          <!-- 树形图表 -->
+          <el-tree
+            :data="data"
+            show-checkbox
+            node-key="id"
+            default-expand-all
+            :expand-on-click-node="false"
+          >
+            <span class="custom-tree-node" slot-scope="{ node, data }">
+              <span>{{ node.label }}</span>
+              <span>
+                <el-button type="text" size="mini" @click="() => append(data)">Append</el-button>
+                <el-button type="text" size="mini" @click="() => remove(node, data)">Delete</el-button>
+              </span>
+            </span>
+          </el-tree>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+       data() {
+             return {
+                   
+             }
+       },
+};
+</script>
+
+<style>
+</style>
